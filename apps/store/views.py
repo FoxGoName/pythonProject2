d3619c8d85961a8d81beac5a3fb67c4852f100cb
FoxGoName <106571781+FoxGoName@users.noreply.github.com>
2024-03-02 14:20:56 +0800

    commit

---
+++ apps/store/views.py
@@ -0,0 +1,137 @@
+import random
+
+from datetime import datetime
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.urls import reverse, reverse_lazy
+
+from apps.cart.cart import Cart
+
+from .models import Product, Category, ProductReview
+from django.views.generic import CreateView, UpdateView, DeleteView
+
+def search(request):
+    query = request.GET.get('query')
+    instock = request.GET.get('instock')
+    price_from = request.GET.get('price_from', 0)
+    price_to = request.GET.get('price_to', 100000)
+    sorting = request.GET.get('sorting', '-date_added')
+    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query)).filter(price__gte=price_from).filter(price__lte=price_to)
+
+    if instock:
+        products = products.filter(num_available__gte=1)
+
+    context = {
+        'query': query,
+        'products': products.order_by(sorting),
+        'instock': instock,
+        'price_from': price_from,
+        'price_to': price_to,
+        'sorting': sorting
+    }
+
+    return render(request, 'search.html', context)
+
+def product_detail(request, category_slug, slug):
+    product = get_object_or_404(Product, slug=slug)
+    product.num_visits = product.num_visits + 1
+    product.last_visit = datetime.now()
+    product.save()
+
+    # Add review
+
+    if request.method == 'POST' and request.user.is_authenticated:
+        stars = request.POST.get('stars', 3)
+        content = request.POST.get('content', '')
+
+        review = ProductReview.objects.create(product=product, user=request.user, stars=stars, content=content)
+
+        return redirect('product_detail', category_slug=category_slug, slug=slug)
+
+    #
+
+    related_products = list(product.category.products.filter(parent=None).exclude(id=product.id))
+    
+    if len(related_products) >= 3:
+        related_products = random.sample(related_products, 3)
+
+    if product.parent:
+        return redirect('product_detail', category_slug=category_slug, slug=product.parent.slug)
+
+    imagesstring = "{'thumbnail': '%s', 'image': '%s'}," % (product.thumbnail.url, product.image.url)
+
+    for image in product.images.all():
+        imagesstring = imagesstring + ("{'thumbnail': '%s', 'image': '%s'}," % (image.thumbnail.url, image.image.url))
+
+    cart = Cart(request)
+
+    if cart.has_product(product.id):
+        product.in_cart = True
+    else:
+        product.in_cart = False
+
+    context = {
+        'product': product,
+        'imagesstring': imagesstring,
+        'related_products': related_products
+    }
+
+    return render(request, 'product_detail.html', context)
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = category.products.filter(parent=None)
+
+    context = {
+        'category': category,
+        'products': products
+    }
+
+    return render(request, 'category_detail.html', context)
+
+
+class productCreateView(CreateView):
+    model = Product
+    template_name = "add_product.html"
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse("frontpage")
+
+class editProductView(UpdateView):
+    model = Product
+    template_name = "edit_product.html"
+    fields = '__all__'
+
+    def get_success_url(self):
+        product = self.object  # 获取正在编辑的产品对象
+        return reverse("product_detail", kwargs={'category_slug': product.category.slug, 'slug': product.slug})
+
+class deleteProductView(DeleteView):
+    model = Product
+    template_name = "delete_product.html"
+    success_url = reverse_lazy('frontpage')
+
+
+# def search_Manage(request):
+#     query = request.GET.get('query')  # 获取搜索关键词
+
+#     if query:
+#         results = Product.objects.filter(title__icontains=query)  # 使用icontains过滤器进行模糊匹配
+#     else:
+#         results = Product.objects.all()  # 如果没有搜索关键词，返回所有对象
+
+#     return render(request, 'search_manage.html', {'results': results, 'query': query})
+    
+def search_Manage(request):
+    query = request.GET.get('query')  # 获取搜索关键词
+
+    if query:
+        results = Product.objects.filter(title__icontains=query)  # 使用icontains过滤器进行模糊匹配
+    else:
+        results = Product.objects.all()  # 如果没有搜索关键词，返回所有对象
+
+    products = results  # 将results赋值给products变量
+
+    return render(request, 'search_manage.html', {'results': results, 'query': query, 'products': products})
---
+++ saulgadgets/urls.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.auth import views
+
+from apps.cart.webhook import webhook
+from apps.cart.views import cart_detail, success
+from apps.core.views import frontpage, contact, about
+from apps.order.views import admin_order_pdf
+from apps.store.views import product_detail, category_detail, search, search_Manage
+from apps.userprofile.views import signup, myaccount
+
+from apps.newsletter.api import api_add_subscriber
+from apps.store.api import api_add_to_cart, api_remove_from_cart, create_checkout_session, validate_payment
+from apps.core.views import productManagePage
+from .sitemaps import StaticViewSitemap, CategorySitemap, ProductSitemap
+from apps.store.views import productCreateView, editProductView, deleteProductView
+
+sitemaps = {'static': StaticViewSitemap, 'product': ProductSitemap, 'category': CategorySitemap}
+
+urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('search/', search, name='search'),
+    path('search_manage/', search_Manage, name='searchManage'),
+    path('cart/', cart_detail, name='cart'),
+    path('hooks/', webhook, name='webhook'),
+    path('cart/success/', success, name='success'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
+    path('admin/', admin.site.urls),
+    path('admin/admin_order_pdf/<int:order_id>/', admin_order_pdf, name='admin_order_pdf'),
+
+    # Auth
+
+    path('myaccount/', myaccount, name='myaccount'),
+    path('signup/', signup, name='signup'),
+    path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
+    # API
+
+    path('api/create_checkout_session/', create_checkout_session, name='create_checkout_session'),
+    path('api/validate_payment/', validate_payment, name='validate_payment'),
+    path('api/add_to_cart/', api_add_to_cart, name='api_add_to_cart'),
+    path('api/remove_from_cart/', api_remove_from_cart, name='api_remove_from_cart'),
+    path('api/add_subscriber/', api_add_subscriber, name='api_add_subscriber'),
+
+    #projectManager
+    path('productManager/', productManagePage, name="productManager"),
+    path('productManager/add/', productCreateView.as_view(), name="addProduct"),
+    path('productManager/edit/<int:pk>/', editProductView.as_view(), name='editProduct'),
+    path('productManager/delete/<int:pk>/', deleteProductView.as_view(), name='deleteProduct'),
+
+    # Store
+
+    path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
+    path('<slug:slug>/', category_detail, name='category_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
