34753abecd2a8a1c7d4680735ee2cc489a230cc0
Nick-60 <2964186029@qq.com>
2024-03-09 19:45:50 +0800

    modify vendor function

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 from apps.store.models import Product, Category
 
+from django.views.generic import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse, reverse_lazy
+
+from django import forms
+
 def frontpage(request):
     products = Product.objects.all()
     featured_categories = Category.objects.all()
@@ -30,3 +36,31 @@ def contact(request):
 
 def about(request):
     return render(request, 'about.html')
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+class productCreateView(CreateView):
+    model = Product
+    template_name = "add_product.html"
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse("frontpage")
+
+
+
+class editProductView(UpdateView):
+    model = Product
+    template_name = "edit_product.html"
+    fields = '__all__'
+
+    def get_success_url(self):
+        product = self.object  # 获取正在编辑的产品对象
+        return reverse("product_detail", kwargs={'category_slug': product.category.slug, 'slug': product.slug})
+
+class deleteProductView(DeleteView):
+    model = Product
+    template_name = "delete_product.html"
+    success_url = reverse_lazy('frontpage')
