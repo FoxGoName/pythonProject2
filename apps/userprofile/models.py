81c3548ae1584c4065d31a32dafccab4fba77b26
Nick-60 <2964186029@qq.com>
2024-04-06 11:37:46 +0800

    i18n(signup, login, myaccount)

--- apps/userprofile/models.py
+++ apps/userprofile/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext as _
 
 class Userprofile(models.Model):
-    user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
-    address = models.CharField(max_length=255, blank=True, null=True)
-    zipcode = models.CharField(max_length=255, blank=True, null=True)
-    place = models.CharField(max_length=255, blank=True, null=True)
-    phone = models.CharField(max_length=255, blank=True, null=True)
+    user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE, verbose_name=_('user'))
+    address = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('address'))
+    zipcode = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('zipcode'))
+    place = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('place'))
+    phone = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('phone'))
 
     def __str__(self):
         return '%s' % self.user.username
